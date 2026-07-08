2c9f54d05a2f13d960da5d7f054265da11fe4a67
Fudge Breadstein <trubhawkins@gmail.com>
2026-07-07 22:10:46 -0400

    made world

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <FormEngine.h>
+#include <TUI.h>
+#include <AudioMan.h>
+
+int worldX = 80;
+int worldY = 40;
 
 int main() {
+	startWorld(true);
+
+	makeWorld(worldX, worldY);
+	setFrameDimension(worldX, worldY);
+	setFramePosition(worldX/2, worldY/2);
+
+	runWorld();
+
+	endWorld();
 	return 0;
 }
